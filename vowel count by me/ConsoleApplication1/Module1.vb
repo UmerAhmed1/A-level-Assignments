cb08e813060193e9cd903813f1f785baba981881
UmerAhmed1 <35227030+UmerAhmed1@users.noreply.github.com>
2018-03-07 20:12:27 +0500

    Add files via upload

---
+++ vowel count by me/ConsoleApplication1/Module1.vb
@@ -0,0 +1,32 @@
+﻿Module Module1
+
+    Sub Main()
+        'declaration
+        Dim I As Integer
+        Dim novowels As Integer
+        Dim vowels As String
+        Dim word As String
+        'initialization
+        I = 0
+        novowels = 0
+        vowels = ""
+        word = ""
+        'data inputing
+        Console.WriteLine("Enter a word")
+        word = Console.ReadLine
+        'data processing
+        vowels = "aeiou"
+        For I = 1 To Len(word)
+            If InStr(vowels, Mid(word, I, 1)) Then
+                novowels = novowels + 1
+            End If
+        Next I
+        Console.WriteLine("No of vowels in the word= " & novowels)
+        Console.ReadKey()
+
+
+
+
+    End Sub
+
+End Module
